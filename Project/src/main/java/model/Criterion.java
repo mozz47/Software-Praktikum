a65f2d60560b1fa898f0ce00656de9263679d57e
Christian Andreas Voß <vosschri@uni-marburg.de>
2024-05-22 20:38:07 +0200

    added window for ranking the criteria 6-10 added drag and drop functionality for JList via a custom TransferHandler added Criterion Enum for criteria 6-10 started adding language support for criteria ranker deleted CriteriaRegulator.form and .java minor changes

---
+++ Project/src/main/java/model/Criterion.java
@@ -0,0 +1,9 @@
+package model;
+
+public enum Criterion {
+    Criterion_06_Food_Preference,
+    Criterion_07_Age_Difference,
+    Criterion_08_Sex_Diversity,
+    Criterion_09_Path_Length,
+    Criterion_10_Group_Amount
+}
